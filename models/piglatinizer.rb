ece7983669f2b8ce332068184e3c6dc92f32cae8
Austin Asbury <acasbury25@aol.com>
2019-10-26 16:42:04 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,3 +1,10 @@
 class PigLatinizer
-
+  def piglatinize (phrase)
+    phrase_array = phrase.split(' ')
+    phrase_array.map do |word|
+      if word.length == 1
+        word + "way"
+      end
+    end
+  end
 end
